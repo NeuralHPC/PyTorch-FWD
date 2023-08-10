0020de53418e305b2d9944fd626d61290239c301
loki-veera <veeramacheneni.lokesh@proton.me>
2023-08-10 18:59:12 +0200

    Compute FID statistics during sampling

--- scripts/sample_diffuse_celebA.py
+++ scripts/sample_diffuse_celebA.py
@@ -13,6 +13,7 @@ from src.sample import sample_noise
 from src.util import write_movie, _sampler_args, get_label_dict
 from functools import partial
 from tqdm import tqdm
+from src.fid import inception, fid
 
 
 def sample_net_noise(net_state: FrozenDict, model: nn.Module, key: int,
@@ -60,7 +61,7 @@ def sample_net_noise(net_state: FrozenDict, model: nn.Module, key: int,
 
 if __name__ == "__main__":
     args = _sampler_args()
-    batch_size = 1000 # Reduce this number in case of low memory
+    batch_size = 200 # Reduce this number in case of low memory
     with open(args.ckpt_path, "rb") as fp:
         loaded = pickle.load(fp)
    
@@ -68,7 +69,14 @@ if __name__ == "__main__":
     labels = jnp.array(list(labels_dict.values()))
 
     (net_state, opt_state, model) = loaded
-    
+
+    inception_model = inception.InceptionV3(pretrained=True)
+    inception_net_state = inception_model.init(
+        jax.random.PRNGKey(0),
+        jnp.ones((1, 256, 256, 3))
+    )
+    apply_fn = jax.jit(partial(inception_model.apply, train=False))
+
     # Sample 1 image and save as GIF
     if args.gif:
         key = jax.random.PRNGKey(random.randint(0, 10000))
@@ -82,11 +90,27 @@ if __name__ == "__main__":
                              net_state=net_state, model=model,
                              key=args.seed, input_shape=[args.input_shape, args.input_shape, 3],
                              max_steps=args.diff_steps, is_gif=args.gif))
+
+    def rescale(img):
+        img = (img - jnp.min(img))/(jnp.max(img) - jnp.min(img))*255.0
+        return img.astype(jnp.uint8)
+
+
+    batch_activations = []
     for idx in tqdm(range(len(labels)//batch_size)):
         sampled_imgs = sample_partial(test_label=labels[idx*batch_size:(idx+1)*batch_size])
-        root_save_path="sample_imgs"
-        os.makedirs(root_save_path, exist_ok=True)
-        file_name = os.path.join(root_save_path,f"sample_{args.input_shape}x{args.input_shape}_{idx+1}.npy")
-        with open(file_name, 'wb') as fp:
-            jnp.save(fp, sampled_imgs)
-    print("Sampling complete")
+        sampled_imgs = jax.vmap(rescale)(sampled_imgs)
+        acts = fid.compute_sampled_statistics(sampled_imgs,
+                                              inception_net_state,
+                                              apply_fn)
+        batch_activations.append(acts)
+    batch_activations = jnp.concatenate(batch_activations, axis=0)
+    # Compute mean and variance
+    mu = np.mean(batch_activations, axis=0)
+    sigma = np.cov(batch_activations, rowvar=False)
+
+    root_save_path="sample_imgs"
+    os.makedirs(root_save_path, exist_ok=True)
+    fname = os.path.join(root_save_path, f"stats_{args.input_shape}x{args.input_shape}.npz")
+    np.savez(fname, mu=mu, sigma=sigma)
+    print(f"Sampling complete and precomputed mean and sigma for FID computation are saved at {fname}")
