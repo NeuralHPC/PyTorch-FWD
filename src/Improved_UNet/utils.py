75affa172ec64bb808d03ce0635baa0fb64fe97b
loki-veera <veeramacheneni.lokesh@proton.me>
2023-08-17 15:56:22 +0200

    add scale shift norm

--- src/Improved_UNet/utils.py
+++ src/Improved_UNet/utils.py
@@ -6,6 +6,7 @@ import jax
 class ResBlock(nn.Module):
     out_channels: int
     use_conv: bool = False
+    use_scale_shift_norm: bool = True
 
     @nn.compact
     def __call__(self, x: jnp.ndarray, emb: jnp.ndarray) -> jnp.ndarray:
@@ -16,20 +17,27 @@ class ResBlock(nn.Module):
         ])
         emd_layers = nn.Sequential([
             nn.silu,
-            nn.Dense(self.out_channels) # is scale_shift_norm necesarry?
+            # is scale_shift_norm necesarry? - yes it adds stability to training large images.
+            nn.Dense(2 * self.out_channels if self.use_scale_shift_norm else self.out_channels) 
         ])
-        out_layers = nn.Sequential([
+        out_layers = [
             nn.GroupNorm(),
             nn.silu,
             # Dropped the dropout layer here - if necessary add later
             nn.Conv(self.out_channels, [3, 3], padding="SAME") # Torch has these params zeroed out and detached - if necessary add later
-        ])
+        ]
 
         h = in_layers(x)
         emb_out = emd_layers(emb)
         emb_out = jnp.expand_dims(emb_out, axis=[1, 2])
-        h = h + emb_out
-        h = out_layers(h)
+        if self.use_scale_shift_norm:
+            out_norm, out_rest = out_layers[0], nn.Sequential(out_layers[1:])
+            scale, shift = jnp.split(emb_out, 2, axis=-1)
+            h = out_norm(h) * (1 + scale) + shift
+            h = out_rest(h)
+        else:
+            h = h + emb_out
+            h = nn.Sequential(out_layers)(h)
 
         if self.out_channels == x.shape[-1]:
             return x + h
