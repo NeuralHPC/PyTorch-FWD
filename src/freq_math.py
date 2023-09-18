ef1ea3e424911b60d6d16bfc71fa2ffed69d280d
Lokesh Veeramacheneni <veeramacheneni.lokesh@proton.me>
2023-09-19 01:47:58 +0200

    convert freq_math to torch compatible

--- src/freq_math.py
+++ src/freq_math.py
@@ -1,11 +1,10 @@
 from itertools import product
 from typing import Tuple
 
-import jax.numpy as jnp
-import jaxwt as jwt
-import numpy as np
-import optax
+import torch
+import ptwt
 import pywt
+import torch.nn.functional as F
 
 
 def get_freq_order(level: int):
@@ -50,74 +49,63 @@ def get_freq_order(level: int):
     return wp_frequency_path, wp_natural_path
 
 
-def fold_channels(array: jnp.ndarray) -> jnp.ndarray:
+def fold_channels(input_tensor: torch.Tensor) -> torch.Tensor:
     """Fold a trailing (color-) channel into the batch dimension.
 
     Args:
-        array (jnp.ndarray): An array of shape [B, H, W, C]
+        input_tensor (torch.Tensor): An array of shape [B, C, H, W]
 
     Returns:
-        jnp.ndarray: The folded [B*C, H, W] image.
+        torch.Tensor: The folded [B*C, H, W] image.
     """
-    shape = array.shape
-    # fold color channel.
-    return jnp.transpose(
-        jnp.reshape(jnp.transpose(array, [1, 2, 0, 3]), [shape[1], shape[2], -1]),
-        [-1, 0, 1],
-    )
+    shape = input_tensor.shape
+    return torch.reshape(input_tensor, (-1, shape[-2], shape[-1]))
 
 
 def unfold_channels(
-    array: jnp.ndarray, original_shape: Tuple[int, int, int, int]
-) -> jnp.ndarray:
+    input_tensor: torch.Tensor, original_shape: Tuple[int, int, int, int]
+) -> torch.Tensor:
     """Restore channels from the leading batch-dimension.
 
     Args:
-        array (jnp.ndarray): An [B*C, packets, H, W] input array.
+        array (torch.Tensor): An [B*C, packets, H, W] input array.
 
     Returns:
-        jnp.ndarray: Output of shape [B, H, W, C]
+        torch.Tensor: Output of shape [B, packets, H, W, C]
     """
 
-    bc_shape = array.shape
-    array_rs = jnp.reshape(
-        jnp.transpose(array, [1, 2, 3, 0]),
-        [bc_shape[1], bc_shape[2], bc_shape[3], original_shape[0], original_shape[3]],
-    )
-    return jnp.transpose(array_rs, [-2, 0, 1, 2, 4])
+    _, packets, _, _ = input_tensor.shape
+    b, c, h, w = original_shape
+    return torch.reshape(input_tensor, (b, packets, c, h, w))
 
 
 def forward_wavelet_packet_transform(
-    tensor: jnp.ndarray, wavelet: str = "db3", max_level: int = 3, log_scale=False
-) -> jnp.ndarray:
-    shape = tensor.shape
-    # fold color channel.
-    tensor = fold_channels(tensor)
-    packets = jwt.packets.WaveletPacket2D(
-        tensor, pywt.Wavelet(wavelet), max_level=max_level
-    )
-
-    paths = list(product(["a", "h", "v", "d"], repeat=max_level))
+    tensor: torch.Tensor,
+    wavelet: str = "db3",
+    max_level: int = 3,
+    log_scale: bool = False,
+) -> torch.Tensor:
+    
+    packets = ptwt.WaveletPacket2D(tensor, pywt.Wavelet(wavelet), maxlevel=max_level)
     packet_list = []
-    for node in paths:
-        packet_list.append(packets["".join(node)])
-    wp_pt = jnp.stack(packet_list, axis=1)
-
-    # restore color channel
-    wp_pt_rs = unfold_channels(wp_pt, shape)
+    
+    for node in packets.get_natural_order(max_level):
+        packet = torch.squeeze(packets[node], dim=1)
+        packet_list.append(packet)
+    wp_pt_rs = torch.stack(packet_list, axis=1)
 
     if log_scale:
-        wp_pt_rs = jnp.log(jnp.abs(wp_pt_rs) + 1e-12)
+        wp_pt_rs = torch.log(torch.abs(wp_pt_rs) + 1e-12)
 
     return wp_pt_rs
 
 
-def generate_frequency_packet_image(packet_array: np.ndarray, degree: int):
+def generate_frequency_packet_image(packet_tensor: torch.Tensor, degree: int):
     """Create a ready-to-polt image with frequency-order packages.
        Given a packet array in natural order, creat an image which is
        ready to plot in frequency order.
     Args:
-        packet_array (np.ndarray): [packet_no, packet_height, packet_width]
+        packet_array (torch.Tensor): [packet_no, packet_height, packet_width]
             in natural order.
         degree (int): The degree of the packet decomposition.
     Returns:
@@ -131,69 +119,67 @@ def generate_frequency_packet_image(packet_array: np.ndarray, degree: int):
         row = []
         for row_path in row_paths:
             index = wp_natural_path.index(row_path)
-            packet = packet_array[:, index]
+            packet = packet_tensor[:, index]
             row.append(packet)
-        image.append(np.concatenate(row, -2))
-    return np.concatenate(image, -3)
+        image.append(torch.concatenate(row, -2))
+    return torch.concatenate(image, -3)
 
 
 def inverse_wavelet_packet_transform(
-    packet_array: jnp.array, wavelet: str, max_level: int
+    packet_tensor: torch.Tensor, wavelet: str, max_level: int
 ):
-    batch = packet_array.shape[0]
-    channel = packet_array.shape[-1]
-
+    batch, _, channels, _,  _ = packet_tensor.shape
     def get_node_order(level):
         wp_natural_path = list(product(["a", "h", "v", "d"], repeat=level))
         return ["".join(p) for p in wp_natural_path]
-
+    
     wp_dict = {}
     for pos, path in enumerate(get_node_order(max_level)):
-        wp_dict[path] = packet_array[:, pos, :, :, :]
-
+        wp_dict[path] = packet_tensor[:, pos, :, :, :]
+    
     for level in reversed(range(max_level)):
         for node in get_node_order(level):
             data_a = fold_channels(wp_dict[node + "a"])
             data_h = fold_channels(wp_dict[node + "h"])
             data_v = fold_channels(wp_dict[node + "v"])
             data_d = fold_channels(wp_dict[node + "d"])
-            rec = jwt.waverec2(
+            rec = ptwt.waverec2(
                 [data_a, (data_h, data_v, data_d)], pywt.Wavelet(wavelet)
             )
             height = rec.shape[1]
             width = rec.shape[2]
             rec = unfold_channels(
-                np.expand_dims(rec, 1), [batch, height, width, channel]
+                torch.unsqueeze(rec, 1), [batch, channels, height, width]
             )
-            rec = np.squeeze(rec, 1)
+            rec = torch.squeeze(rec, 1)
             wp_dict[node] = rec
     return rec
 
 
-def fourier_power_divergence(output: jnp.ndarray, target: jnp.ndarray) -> jnp.ndarray:
+def fourier_power_divergence(output: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
     """Power spectrum entropy metric as presented in:
     https://openaccess.thecvf.com/content_ICCV_2019/papers/Hernandez_Human_Motion_Prediction_via_Spatio-Temporal_Inpainting_ICCV_2019_paper.pdf
 
     Args:
-        output (jnp.ndarray): The network output.
-        target (jnp.ndarray): The target image.
+        output (torch.Tensor): The network output.
+        target (torch.Tenso): The target image.
 
     Returns:
-        (jnp.ndarray): A scalar metric.
+        (torch.Tensor): A scalar metric.
     """
 
-    radius_no_sqrt = lambda z_comp: jnp.real(z_comp) ** 2 + jnp.imag(z_comp) ** 2
+    radius_no_sqrt = lambda z_comp: torch.real(z_comp) ** 2 + torch.imag(z_comp) ** 2
 
-    output_fft = jnp.fft.fft2(output)
+    output_fft = torch.fft.fft2(output)
     output_power = radius_no_sqrt(output_fft)
-    target_fft = jnp.fft.fft2(target)
+    target_fft = torch.fft.fft2(target)
     target_power = radius_no_sqrt(target_fft)
-    return jnp.mean(optax.convex_kl_divergence(jnp.log(output_power), target_power))
+    return torch.mean(F.kl_div(torch.log(output_power), target_power))
 
 
 def wavelet_packet_power_divergence(
-    output: jnp.ndarray, target: jnp.ndarray, level: int = 3
-) -> jnp.ndarray:
+    output: torch.Tensor, target: torch.Tensor, level: int = 3
+) -> torch.Tensor:
     """Compute the wavelet packet power divergence.
 
     Daubechies wavelets are orthogonal, see Ripples in Mathematics page 129:
@@ -207,15 +193,15 @@ def wavelet_packet_power_divergence(
     Proposition 7.7.1 from Ripples in Mathematics page 80.
 
     Args:
-        output (jnp.ndarray): _description_
-        target (jnp.ndarray): _description_
+        output (torch.Tensor): The network output
+        target (torch.Tensor): The target image
 
     Returns:
-        jnp.ndarray: _description_
+        torch.Tensor: Wavelet power divergence metric
     """
     output_packets = forward_wavelet_packet_transform(output, max_level=level)
     target_packets = forward_wavelet_packet_transform(target, max_level=level)
 
     output_energy = output_packets**2
     target_energy = target_packets**2
-    return jnp.mean(optax.convex_kl_divergence(jnp.log(output_energy), target_energy))
+    return torch.mean(F.kl_div(torch.log(output_energy), target_energy))
