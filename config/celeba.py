132af443f15f102b98837e535d93621a7bdea252
loki-veera <veeramacheneni.lokesh@proton.me>
2023-10-12 11:10:40 +0200

    update mean and std for celeba & celebahq datasets

--- config/celeba.py
+++ config/celeba.py
@@ -6,14 +6,15 @@ celeba_path: str = None
 celebahq_path: str = None
 
 
-
 class CELEBA64:
     def __init__(self) -> None:
         self.dataset_config: Dict[str, Any] = {
             'random_flip': True,
-            'num_workers': 48, # Feel free to change this
+            'num_workers': 48,  # Feel free to change this
             'dataset': 'CELEBA',
             'resize': 64,
+            'mean': [x / 255.0 for x in [129.058, 108.485, 97.622]],
+            'std': [x / 255.0 for x in [78.338, 73.131, 72.970]],
         }
 
         self.model_config: Dict[str, Any] = {
@@ -39,14 +40,15 @@ class CELEBA64:
         self.data_dir: str = celeba_path
 
 
-
 class CELEBAHQ64:
     def __init__(self) -> None:
         self.dataset_config: Dict[str, Any] = {
             'random_flip': True,
-            'num_workers': 48, # Feel free to change this
+            'num_workers': 48,  # Feel free to change this
             'dataset': 'CELEBAHQ',
             'resize': 64,
+            'mean': [x / 255.0 for x in [131.810, 106.258, 92.634]],
+            'std': [x / 255.0 for x in [76.332, 69.183, 67.954]],
         }
 
         self.model_config: Dict[str, Any] = {
@@ -76,9 +78,11 @@ class CELEBAHQ128:
     def __init__(self) -> None:
         self.dataset_config: Dict[str, Any] = {
             'random_flip': True,
-            'num_workers': 48, # Feel free to change this
+            'num_workers': 48,  # Feel free to change this
             'dataset': 'CELEBAHQ',
-            'resize': 128,
+            'resize': None,
+            'mean': [x / 255.0 for x in [131.810, 106.258, 92.634]],
+            'std': [x / 255.0 for x in [76.332, 69.183, 67.954]],
         }
 
         self.model_config: Dict[str, Any] = {
